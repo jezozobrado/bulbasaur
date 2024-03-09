b4b3211ce69047181860e72a37c8635b889bc478
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2024-03-09 10:10:51 +0800

    setup prisma and postgre

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import prisma from "@/lib/prisma";
 
-const page = () => {
-  return <div>Home page</div>;
+const page = async () => {
+  const entries = await prisma.entry.findMany();
+  return (
+    <div>
+      Home page
+      {entries.map((entry) => (
+        <span key={entry.id}>{entry.content}</span>
+      ))}
+    </div>
+  );
 };
 
 export default page;
